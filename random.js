346fd4b7432d5c48b8a25d212877e5b6504814ad
TZANTOU <tzantou286@gmail.com>
2024-11-03 22:16:59 +0200

    mobile apearance

--- random.js
+++ random.js
@@ -3,15 +3,33 @@ const loadRandomMoment = async () => {
         const response = await fetch('data/moments.json');
         const data = await response.json();
         const moments = data.moments;
-
+        
+        
         if (moments.length > 0) {
-            const randomMoment = moments[Math.floor(Math.random() * moments.length)];
-            displayMoment(randomMoment);
+            const today = new Date();
+            const todayString = today.toISOString().slice(0, 10);
+            const savedMomentIndex = localStorage.getItem('dailyMomentIndex');
+            const savedDate = localStorage.getItem('dailyMomentDate');
+
+            if (savedDate === todayString && savedMomentIndex !== null) {
+                displayMoment(moments[savedMomentIndex]);
+            }
+
+            // Δημιουργούμε έναν ημερήσιο δείκτη με βάση την ημερομηνία
+            const dayIndex = today.getFullYear() + today.getMonth() + today.getDate();
+            const momentIndex = dayIndex % moments.length;
+
+            // Αποθηκεύουμε το τυχαίο στιγμιότυπο για την τρέχουσα ημέρα
+            localStorage.setItem('dailyMomentIndex', momentIndex);
+            localStorage.setItem('dailyMomentDate', todayString);
+
+            displayMoment(moments[momentIndex]);
         }
     } catch (error) {
         console.error("Error loading moments:", error);
     }
 };
+
 
 const displayMoment = (moment) => {
     const container = document.getElementById('random-moment');
